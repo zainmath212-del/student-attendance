2d3b393e0528e689497fd8b94909887d06f73af4
zainmath212-del <zainmath212@gmail.com>
2026-07-31 23:06:19 +0700

    Update api.js

--- js/api.js
+++ js/api.js
@@ -1,1 +1,47 @@
+// =====================================
+// KONFIGURASI API
+// =====================================
 
+const API_URL =
+"https://script.google.com/macros/s/AKfycbzxRG0MglizzdThP38rGaAmuIQcZeGM0Xtu-4fwbOBM2u4YVhhdqfVWonU8sdYJgAWUtQ/exec";
+
+
+// =====================================
+// KIRIM DATA ABSENSI
+// =====================================
+
+async function sendAttendance(id){
+
+    try{
+
+        const response = await fetch(API_URL,{
+
+            method:"POST",
+
+            headers:{
+                "Content-Type":"application/json"
+            },
+
+            body:JSON.stringify({
+                id:id
+            })
+
+        });
+
+        const result = await response.json();
+
+        return result;
+
+    }catch(err){
+
+        return{
+
+            success:false,
+
+            message:"Tidak dapat terhubung ke server."
+
+        };
+
+    }
+
+}
